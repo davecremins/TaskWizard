9649a41b3799000394741debda8784037f33e2c8
Dave Cremins <dave.cremins@intel.com>
2019-12-20 15:51:08 +0000

    Move TODO api into manager package

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/davecremins/ToDo-Manager/content"
-	"github.com/davecremins/ToDo-Manager/dates"
+	"github.com/davecremins/ToDo-Manager/manager"
 	. "github.com/davecremins/ToDo-Manager/utilities"
 	"log"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -19,43 +16,5 @@ func main() {
 		log.Fatalf("failed opening file: %s", err)
 	}
 
-	stats, _ := file.Stat()
-	size := stats.Size()
-	log.Println("Size of file:", size)
-
-	contentContainingStr := content.FindSearchStr(file, size, config.SearchStr)
-
-	log.Println("Content found.")
-	fmt.Println("")
-	fmt.Println(contentContainingStr)
-
-	dateStr, err := dates.FindDate(contentContainingStr)
-	if err != nil {
-		panic("Failed to find date in content")
-	}
-
-	datetime, err := dates.ConvertToTime(dateStr)
-	if err != nil {
-		panic("Failed to convert date to time format")
-	}
-
-	datetime = dates.AddDays(datetime, config.DaysToAdd)
-	newDateStr := dates.ExtractShortDate(datetime)
-	newContent := strings.ReplaceAll(contentContainingStr, dateStr, newDateStr)
-
-	log.Println("Content updated with new date")
-	fmt.Println("")
-	fmt.Println(newContent)
-
-	file.Seek(0, 2)
-	_, err = file.Write([]byte("\n\n"))
-	if err != nil {
-		panic("Falied to write newlines to file")
-	}
-
-	_, err = file.Write([]byte(newContent))
-	if err != nil {
-		panic("Falied to write new content to file")
-	}
-
+	manager.CopyPreviousContent(config, file)
 }
