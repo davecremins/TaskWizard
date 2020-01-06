5b69f252cb59a3f777507ec168a56acbd2d7712b
Dave Cremins <dave.cremins@intel.com>
2020-01-06 20:51:50 +0000

    Refactor sub-command creation

--- cli/actions.go
+++ cli/actions.go
@@ -2,47 +2,47 @@ package actions
 
 import (
 	"flag"
-	"fmt"
 	"github.com/davecremins/ToDo-Manager/manager"
 	. "github.com/davecremins/ToDo-Manager/utilities"
 	"log"
 	"os"
 )
 
+var actionMap map[string]func(*Config)
+
 func Process(args []string, defaultConfig *Config) {
 	if len(args) < 2 {
 		log.Fatal("expected subcommands to perform an action")
 	}
 
+	actionMap = make(map[string]func(*Config))
+
 	initCmd := flag.NewFlagSet("init", flag.ExitOnError)
 	filename := initCmd.String("filename", defaultConfig.Filename, "Name of file to initialise")
+	actionMap["init"] = func(config *Config) {
+		initCmd.Parse(args[2:])
+		defaultConfig.Filename = *filename
+		log.Println("Config over-written for init action")
+		log.Println("No implementation provided")
+	}
 
 	newCmd := flag.NewFlagSet("newday", flag.ExitOnError)
 	searchStr := newCmd.String("search", defaultConfig.SearchStr, "Search string to look for")
 	daysToAdd := newCmd.Int("days", defaultConfig.DaysToAdd, "Total amount of days to increment by")
-
-	switch args[1] {
-
-	case "init":
-		initCmd.Parse(args[2:])
-		fmt.Println("subcommand 'init'")
-		fmt.Println("  filename:", *filename)
-		fmt.Println("  tail:", initCmd.Args())
-		defaultConfig.Filename = *filename
-		log.Println("Config over-written for init action")
-	case "newday":
+	actionMap["newday"] = func(config *Config) {
 		newCmd.Parse(args[2:])
-		fmt.Println("subcommand 'newday'")
-		fmt.Println("  searchStr:", *searchStr)
-		fmt.Println("  days:", *daysToAdd)
-		fmt.Println("  tail:", newCmd.Args())
 		defaultConfig.SearchStr = *searchStr
 		defaultConfig.DaysToAdd = *daysToAdd
 		log.Println("Config over-written for newday action")
 		newDayAction(defaultConfig)
-	default:
+
+	}
+
+	action, ok := actionMap[args[1]]
+	if !ok {
 		log.Fatal(args[1] + " subcommand is not supported right now :(")
 	}
+	action(defaultConfig)
 }
 
 func newDayAction(config *Config) {
