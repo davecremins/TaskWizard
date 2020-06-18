53090c7f6401c1e4ff3c05c8eadc4d8bfd95330b
Dave Cremins <davecremins@gmail.com>
2020-06-18 23:54:14 +0100

    Some boundary tests for Move function

--- content/organise_test.go
+++ content/organise_test.go
@@ -157,11 +157,115 @@ Completed 13/01/2020
 Finished a book`
 
 	organisedContent := NewOrganisedContent(content)
-	organisedContent.MoveTODO(2,1)
+	organisedContent.MoveTODO(2, 1)
 	organisedContent.MergeContent()
 
 	if want != organisedContent.MergedContent {
 		t.Errorf("Merged content is incorrect got %s, want %s", organisedContent.MergedContent, want)
 		t.Logf("Size of got %d and want %d", len(organisedContent.MergedContent), len(want))
 	}
+}
+
+func TestMoveTODOItemForwardToNewPositionInTODOItems(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+Finish laying the fence
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	want := `TODOs 13/01/2020
+================
+Write some code
+Finish laying the fence
+Do Something in the evening
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MoveTODO(1, 3)
+	organisedContent.MergeContent()
+
+	if want != organisedContent.MergedContent {
+		t.Errorf("Merged content is incorrect got %s, want %s", organisedContent.MergedContent, want)
+		t.Logf("Size of got %d and want %d", len(organisedContent.MergedContent), len(want))
+	}
+}
+
+func TestMoveTODOItemToSamePositionInTODOItems(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+Finish laying the fence
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	want := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+Finish laying the fence
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MoveTODO(2, 2)
+	organisedContent.MergeContent()
+
+	if want != organisedContent.MergedContent {
+		t.Errorf("Merged content is incorrect got %s, want %s", organisedContent.MergedContent, want)
+		t.Logf("Size of got %d and want %d", len(organisedContent.MergedContent), len(want))
+	}
+}
+
+func TestMoveTODOPanicsWhenToDoItemIsOutOfBounds(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with bad input")
+		}
+		t.Log(r)
+	}()
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MoveTODO(3, 5)
+}
+
+func TestMoveTODOPanicsWhenNewToDoPositionIsOutOfBounds(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with bad input")
+		}
+		t.Log(r)
+	}()
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MoveTODO(1, 3)
 }
