fcba51adb627141fd27da7105f3dcd7e600040aa
Dave Cremins <dave.cremins@intel.com>
2020-07-21 16:08:50 +0100

    Add some more test cases for merge option including negative bounds cases

--- content/organise_test.go
+++ content/organise_test.go
@@ -299,3 +299,88 @@ Finished a book`
 		t.Logf("Size of got %d and want %d", len(organisedContent.MergedContent), len(want))
 	}
 }
+
+func TestMergeTODOsPanicsWhenToDoPositionIsOutOfBounds(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with bad input")
+		}
+		t.Log(r)
+	}()
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MergeTODOs(7, 1)
+}
+
+func TestMergeTODOsPanicsWhenToDoDestinationPositionIsOutOfBounds(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with bad input")
+		}
+		t.Log(r)
+	}()
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MergeTODOs(1, 3)
+}
+
+func TestMergeTODOsPanicsWhenToDoPositionIsOutOfBoundsNegative(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with bad input")
+		}
+		t.Log(r)
+	}()
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MergeTODOs(-1, 3)
+}
+
+func TestMergeTODOsPanicsWhenToDoDestinationPositionIsOutOfBoundsNegative(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with bad input")
+		}
+		t.Log(r)
+	}()
+	organisedContent := NewOrganisedContent(content)
+	organisedContent.MergeTODOs(1, -3)
+}
+
