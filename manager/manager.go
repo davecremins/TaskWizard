f72c191ac4171c6a8354779b3dbce65c547c0f19
Dave Cremins <dave.cremins@intel.com>
2020-07-22 17:29:35 +0100

    Remove redundant new item code

--- manager/manager.go
+++ manager/manager.go
@@ -2,41 +2,11 @@ package manager
 
 import (
 	. "github.com/davecremins/ToDo-Manager/config"
-	"github.com/davecremins/ToDo-Manager/content"
 	"github.com/davecremins/ToDo-Manager/dates"
 	"log"
 	"os"
 	"strings"
 )
-
-func AddNewItem(config *ToDoConfig, file *os.File, newItem string) {
-	stats, _ := file.Stat()
-	size := stats.Size()
-	log.Println("Size of file:", size)
-
-	contentContainingStr := content.FindSearchStr(file, size, "Completed")
-	contentSize := len(contentContainingStr)
-	log.Println("Position found:", contentSize)
-	// Account for newline
-	contentSize += 1
-
-	writingPos := size - int64(contentSize)
-	file.Seek(writingPos, 0)
-	_, err := file.Write([]byte(newItem))
-	if err != nil {
-		panic("Falied to write new item to file")
-	}
-
-	_, err = file.Write([]byte("\n\n"))
-	if err != nil {
-		panic("Falied to write new line to file")
-	}
-
-	_, err = file.Write([]byte(contentContainingStr))
-	if err != nil {
-		panic("Falied to write original content to file")
-	}
-}
 
 func WriteUpdatedContent(file *os.File, originalContentSize int, newContent string) {
 	stats, _ := file.Stat()
