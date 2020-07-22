a4ca1091e8910e958a83b7c0b32ecfb3553010ef
Dave Cremins <dave.cremins@intel.com>
2020-07-22 14:58:14 +0100

    New test to capture when search term is not found

--- content/content_test.go
+++ content/content_test.go
@@ -19,3 +19,24 @@ Finished a book`
 	result := FindSearchStr(reader, int64(len(content)), "Completed")
 	t.Logf("Search result: %s", result)
 }
+
+func TestContentSearchPanicsWhenSearchTermNotFound(t *testing.T) {
+	content := `TODOs 13/01/2020
+================
+Do Something in the evening
+Write some code
+
+Completed 13/01/2020
+====================
+Finished a book`
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Should panic with fulfillment error")
+		}
+		t.Log(r)
+	}()
+	reader := strings.NewReader(content)
+	FindSearchStr(reader, int64(len(content)), "Done")
+}
